b15b43535543f4a0093ed26896c5e2ee2d971dc3
google-automerger <automerger@google.com>
2014-11-18 16:20:20 -0800

    Auto-update

--- Application/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Application/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -47,10 +47,12 @@ public class MainActivity extends SampleActivityBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        RecyclerViewFragment fragment = new RecyclerViewFragment();
-        transaction.replace(R.id.sample_content_fragment, fragment);
-        transaction.commit();
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            RecyclerViewFragment fragment = new RecyclerViewFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
     }
 
     @Override
